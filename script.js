667c168c47ef7d10996c0fff657f0cadf4e8d559
mady12tripathi <mady.12tripathi@gmail.com>
2025-07-05 01:58:22 +0530

    Final Audia+ Website Update

--- script.js
+++ script.js
@@ -8,47 +8,34 @@ function showSlide(index) {
     indicators[i].classList.toggle("active", i === index);
   });
 }
-
 function nextSlide() {
   currentSlide = (currentSlide + 1) % slides.length;
   showSlide(currentSlide);
 }
-
 function prevSlide() {
   currentSlide = (currentSlide - 1 + slides.length) % slides.length;
   showSlide(currentSlide);
 }
-
-// Swipe support for mobile
 let startX = 0;
 document.querySelector(".carousel").addEventListener("touchstart", e => {
   startX = e.touches[0].clientX;
 });
-
 document.querySelector(".carousel").addEventListener("touchend", e => {
   const endX = e.changedTouches[0].clientX;
-  if (startX - endX > 50) nextSlide();       // swipe left
-  else if (endX - startX > 50) prevSlide();  // swipe right
+  if (startX - endX > 50) nextSlide();
+  else if (endX - startX > 50) prevSlide();
 });
-
-// Auto scroll
 let interval = setInterval(nextSlide, 4000);
-
-// Pause on hover
 document.querySelector(".carousel").addEventListener("mouseenter", () => {
   clearInterval(interval);
 });
 document.querySelector(".carousel").addEventListener("mouseleave", () => {
   interval = setInterval(nextSlide, 4000);
 });
-
-// Indicator click
 indicators.forEach((dot, index) => {
   dot.addEventListener("click", () => {
     currentSlide = index;
     showSlide(currentSlide);
   });
 });
-
-// Show initial
 showSlide(currentSlide);
