707f383b6ceda2b0708e305e9a973d7c8d0cf311
mady12tripathi <mady.12tripathi@gmail.com>
2025-07-05 18:02:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-// Auto-scroll banner
-let carousel = document.querySelector('.carousel');
-let banners = document.querySelectorAll('.carousel img');
-let index = 0;
+// script.js
 
-function scrollBanners() {
-  index = (index + 1) % banners.length;
-  carousel.scrollTo({
-    left: banners[index].offsetLeft,
-    behavior: 'smooth'
-  });
+let currentIndex = 0;
+
+function nextSlide() {
+  const carousel = document.querySelector(".carousel-container");
+  currentIndex = (currentIndex + 1) % 4;
+  carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
-setInterval(scrollBanners, 4000);
 
-// Pause on hover
-carousel.addEventListener("mouseover", () => clearInterval(scrollInterval));
-carousel.addEventListener("mouseout", () => scrollInterval = setInterval(scrollBanners, 4000));
+setInterval(nextSlide, 4000);
 
-// Modal Clickable Info Strip
-const infoItems = document.querySelectorAll(".info-item");
-infoItems.forEach(item => {
-  item.addEventListener("click", () => {
-    alert(`You clicked on: ${item.innerText}`);
-  });
-});
+// Info Modal Popup (if added later)
+function openModal(text) {
+  alert(text);
+}
