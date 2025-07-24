2850ae00f6e2c3671085041c8187db8b9443b1af
mady12tripathi <mady.12tripathi@gmail.com>
2025-07-24 19:36:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const navLinks = document.querySelectorAll('.navbar a');
+  const currentPage = window.location.pathname.split('/').pop();
 
+  navLinks.forEach(link => {
+    if (link.getAttribute('href') === currentPage) {
+      link.classList.add('active');
+    }
+  });
+});
